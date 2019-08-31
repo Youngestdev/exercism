e1c40dcccc450285d22deae8b74b108874dc5e22
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-08-31 09:43:18 +0100

    Format code!

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -6,8 +6,8 @@ package twofer
 // ShareWith should have a comment documenting it.
 // ShareWith function takes a name which is a string to substitue the [you] above.
 
-func ShareWith(name string) string {
-		if name != "" {
+func ShareWith(name string) string {		
+	if name != "" {
 		return "One for " + name+ ", one for me."
 	} else {
 		return "One for you, one for me."
