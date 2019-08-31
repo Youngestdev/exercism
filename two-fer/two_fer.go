da356691cf51145455205d58e5d725adad92f5d8
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-08-31 09:34:43 +0100

    Exercism two-fer exercise

---
+++ two-fer/two_fer.go
@@ -0,0 +1,15 @@
+// https://golang.org/doc/effective_go.html#commentary
+// package twofer implements the "One for [you], one for me."
+
+package twofer
+
+// ShareWith should have a comment documenting it.
+// ShareWith function takes a name which is a string to substitue the [you] above.
+
+func ShareWith(name string) string {
+		if name != "" {
+		return "One for " + name+ ", one for me."
+	} else {
+		return "One for you, one for me."
+	}
+}
