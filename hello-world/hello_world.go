8438710ed4af9ba2f196fa35897424cd629e9e82
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-08-31 09:33:27 +0100

    First exercise

---
+++ hello-world/hello_world.go
@@ -0,0 +1,9 @@
+// Package greeting should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+
+// package greeting's HelloWorld function will return "Hello, World!"
+package greeting
+
+func HelloWorld() string {
+	return "Hello, World!"
+}
